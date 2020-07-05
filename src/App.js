b26aec8c76308bdc7fa1d6446b12cafd78d769f0
Kevin Araújo <kfa_34@hotmail.com>
2020-07-04 22:52:49 -0300

    Using props correctly.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,36 @@ import './App.css';
 import Tabela from './Tabela';
 
 function App() {
+  const authors = [
+    {
+      id: 1,
+      name: 'Paulo',
+      book: 'React',
+      price: '1000'
+    },
+    {
+      id: 2,
+      name: 'Daniel',
+      book: 'Java',
+      price: '99'
+    },
+    {
+      id: 3,
+      name: 'Marcos',
+      book: 'Design',
+      price: '150'
+    },
+    {
+      id: 4,
+      name: 'Bruno',
+      book: 'Devops',
+      price: '100'
+    },
+  ];
+
   return (
     <div className="App">
-      <Tabela/>
+      <Tabela authors={authors} />
     </div>
   );
 }
--- src/Tabela.js
+++ src/Tabela.js
@@ -13,37 +13,34 @@ const TableHead = () => {
     );
 }
 
-const TableBody = () => {
+const TableBody = props => {
+    const lines = props.authors.map((line, index) => {
+        return (
+            <tr>
+                <td>{line.name}</td>
+                <td>{line.book}</td>
+                <td>{line.price}</td>
+                <td><button>Remover</button></td>
+            </tr>
+        );
+    });
+
     return (
         <tbody>
-        <tr>
-            <td>Paulo</td>
-            <td>React</td>
-            <td>1000</td>
-            <td><button>Remover</button></td>
-        </tr>
-        <tr>
-            <td>Nico</td>
-            <td>React</td>
-            <td>1000</td>
-            <td><button>Remover</button></td>
-        </tr>
-        <tr>
-            <td>Daniel</td>
-            <td>React</td>
-            <td>1000</td>
-            <td><button>Remover</button></td>
-        </tr>
+            {lines}
         </tbody>
     );
 }
 
 class Tabela extends Component {
     render(){
+        
+        const { authors } = this.props;
+
         return (
             <table>
                 <TableHead/>
-                <TableBody/>
+                <TableBody authors = {authors} />
             </table>
         );
     }
